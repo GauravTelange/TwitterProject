a2590f64ed8c0ac6fa94f5d36e6494e368eb03a0
Gaurav Telange <gauravtelange4321@gmail.com>
2026-04-18 17:14:08 +0530

    Added Controller and Routes for createTweet api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
 import {connect} from './config/database.js';
+import apiRoutes from './routes/index.js';
 const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/api',apiRoutes);
 
 import service from './services/tweet-service.js';
 
