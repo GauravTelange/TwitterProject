1e9e80e70b8198c6626cf81bc3d2c7c451724663
Gaurav Telange <gauravtelange4321@gmail.com>
2026-04-18 16:14:23 +0530

    Moved to ES Modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-const express = require('express');
-const connect = require('./config/database');
+import express from 'express';
+import {connect} from './config/database.js';
 const app = express();
+
+import service from './services/tweet-service.js';
+
 
 app.listen(3000, async() => {
     console.log('Server Started on Port : 3000');
@@ -9,6 +12,9 @@ app.listen(3000, async() => {
     await connect();
     console.log('Mongodb Connected');
     
+    let ser= new service();
+
+    await ser.create({content: 'Done with #refactor'});
 
    
     
