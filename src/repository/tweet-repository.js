aff611c8faa2a0b7bf45bb2dc33cea2bee99f399
Gaurav Telange <gauravtelange4321@gmail.com>
2026-04-30 14:05:14 +0530

    some fix

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,56 +1,67 @@
-import Tweet from  '../models/tweet.js';
+import Tweet from '../models/tweet.js';
 import CrudRepository from './crud-repository.js';
+import Hashtag from '../models/hashtags.js';
 
+class TweetRepository extends CrudRepository {
+    constructor(){
+        super(Tweet);
+    }
 
-class TweetRepository extends CrudRepository{
-    
-        constructor(){
-            super(Tweet);
+    async create(data){
+        try {
+            const tweet = await Tweet.create(data);
+            return tweet;
+        } catch (error) {
+            console.log(error);
         }
+    }
 
-        async create(data){
-            try {
-                const tweet = await Tweet.create(data);
-                return tweet;
-            } catch (error) {
-                console.log(error);
-                
-            }
+    async getWithComments(id){
+        try {
+            const tweet = await Tweet.findById(id).populate({path:'comments'}).lean();
+            return tweet;
+        } catch (error) {
+            console.log(error);
+        }            
+    }
+
+    async find(id){
+        try {
+            const tweet = await Tweet.findById(id).populate({path: 'likes'});
+            return tweet;
+        } catch (error) {
+            console.log(error);
         }
+    }
 
-        
-
-        async getWithComments(id){
-            try {
-                const tweet = await Tweet.findById(id).populate({path:'comments'}).lean();
-                return tweet;
-                
-            } catch (error) {
-                console.log(error);
-                
-            }            
+    async getAll(offset, limit){
+        try {
+            const tweet = await Tweet.find().skip(offset).limit(limit);
+            return tweet;
+        } catch (error) {
+            console.log(error);
         }
-
-        
-        async find(id){
-            try {
-                const tweet = await Tweet.findById(id).populate({path: 'likes'});
-                return tweet;
-            } catch (error) {
-                console.log(error);
-                
-            }
-        }
-
-        async getAll(offset, limit){
-            try {
-                const tweet = await Tweet.find().skip(offset).limit(limit);
-                return tweet;
-            } catch (error) {
-                console.log(error);
-                
-            }
-        }
-
+    }
 }
-export default TweetRepository;
+
+class HashTagRepository {
+    async findByName(tags){
+        try {
+            const hashtags = await Hashtag.find({ title: { $in: tags } });
+            return hashtags;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async bulkCreate(tags){
+        try {
+            const hashtags = await Hashtag.insertMany(tags);
+            return hashtags;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+export { TweetRepository, HashTagRepository };
